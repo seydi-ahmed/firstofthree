b97502c46d27ec243718575a14243582a2327745
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-08-27 22:44:49 +0000

    initialisation du projet avec les fichiers: - push.sh --> vers git - .gitignore --> ignorer certains fichiers - README --> explciation

--- push.sh
+++ push.sh
@@ -1,4 +1,11 @@
 git add .
-git commit -m "initialisation"
+
+git commit -m "
+initialisation du projet avec les fichiers:
+- push.sh --> vers git
+- .gitignore --> ignorer certains fichiers
+- README --> explciation
+"
+
 git push gitea
 git push github
