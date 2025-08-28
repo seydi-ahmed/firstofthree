2a8b9ad029187aef2a04f070032f1daa988c704e
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-08-28 19:04:48 +0000

    TaskRepository.java complet et compris

--- backend/src/main/java/com/example/repository/TaskRepository.java
+++ backend/src/main/java/com/example/repository/TaskRepository.java
@@ -1,5 +1,12 @@
 package com.example.repository;
 
-public class TaskRepository {
-    
+import com.example.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+   List<Task> findByName(String name);
 }
