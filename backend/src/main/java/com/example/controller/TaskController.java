24fa277589a950cd13052c4b443c7e53d3b18a5e
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-08-30 14:48:38 +0000

    TaskController.java complet et compris

--- backend/src/main/java/com/example/controller/TaskController.java
+++ backend/src/main/java/com/example/controller/TaskController.java
@@ -1,5 +1,51 @@
 package com.example.controller;
 
+import com.example.model.Task;
+import com.example.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController                   // Indique que c'est un contrôleur REST
+@RequestMapping("/tasks")         // Toutes les routes commencent par /tasks
 public class TaskController {
-    
+
+    @Autowired
+    private TaskService taskService;
+
+    // Récupérer toutes les tâches
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    // récupérer une tache
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id){
+        return taskService.getTaskById(id).orElse(null);
+    }
+
+    // créer une nouvelle tache
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskService.saveTask(task);
+    }
+
+    // mettre à jour
+    public Task updTask(@PathVariable Long id, @RequestBody Task updatedTask){
+        Task task = taskService.getTaskById(id).orElse(null);
+        if (task != null){
+            task.setName(updatedTask.getName());
+            task.setDescription(updatedTask.getDescription());
+            return updatedTask;
+        }
+        return null;
+    }
+
+    // supprimer une tache
+    @PutMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+    }
 }
